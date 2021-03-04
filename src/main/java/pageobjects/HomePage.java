a410bd7b9894cbc015a3c614e5e7265d8817914a
Vladislav Murashov <vmurashov@luxoft.com>
2021-03-04 16:25:21 +0300

    cleared

--- src/main/java/pageobjects/HomePage.java
+++ src/main/java/pageobjects/HomePage.java
@@ -2,93 +2,30 @@ package pageobjects;
 
 import common.Init;
 import common.ParentPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import pageobjects.project.AllProjectsPage;
-import pageobjects.project.ProjectPage;
-
-import java.util.List;
 
 public class HomePage extends ParentPage {
 
     @FindBy(xpath = "//span[contains(text(), 'My Projects ')]")
-    private WebElement myProjectsHeader;
+    private WebElement searchArea;
 
     @FindBy(xpath = "//a[@title='View All Project']")
-    private WebElement viewAllProjects;
+    private WebElement searchButton;
 
-    @FindBy(xpath = "//div[@display-name='Project Name']//input[@placeholder='Search by Name']")
-    private WebElement searchByName;
-
-    @FindBy(xpath = "//div[@display-name='Project Code']//input[@placeholder='Search by Project Code']")
-    private WebElement searchByProjectCode;
-
-    @FindBy(xpath = "//div[@display-name='API ID']//input[@placeholder='Search by API ID']")
-    private WebElement searchByApiId;
-
-    @FindBy(xpath = "//div[@display-name='Last Updated']//input[@placeholder='Select Date Range']")
-    private WebElement selectDateRange;
-
-
-    HomePage() {
-
-        PageFactory.initElements(Init.getWebDriver(), this);
-        new WebDriverWait(Init.getWebDriver(), Init.getTimeOut()).until(ExpectedConditions.visibilityOf(myProjectsHeader));
-    }
-
-    public HomePage(String user) {
+    public HomePage() {
 
         Init.getWebDriver().navigate().to("yandex.ru");
         PageFactory.initElements(Init.getWebDriver(), this);
-        new WebDriverWait(Init.getWebDriver(), Init.getTimeOut()).until(ExpectedConditions.visibilityOf(myProjectsHeader));
+        new WebDriverWait(Init.getWebDriver(), Init.getTimeOut()).until(ExpectedConditions.visibilityOf(searchArea));
     }
 
-    public TopMenuPage goToTopMenu() {
+    public HomePage search(String name) {
 
-        return new TopMenuPage();
-    }
-
-    public HomePage filterByProjectName(String name) {
-
-        setTextValue(searchByName, name);
+        setTextValue(searchArea, name);
         return this;
     }
-
-    public HomePage filterByProjectCode(String code) {
-
-        setTextValue(searchByProjectCode, code);
-        return this;
-    }
-
-    public HomePage filterByApiId(String apiId) {
-
-        setTextValue(searchByApiId, apiId);
-        return this;
-    }
-
-    public HomePage filterByLastUpdated(String date) {
-
-        setTextValue(selectDateRange, date);
-        return this;
-    }
-
-    public ProjectPage openFilteredProject() {
-
-        List<WebElement> projectNames = Init.getWebDriver().findElements(By.xpath("//td[@data-title-text='Project Name']/div/a"));
-        projectNames.get(0).click();
-
-        return new ProjectPage();
-    }
-
-    public AllProjectsPage goToAllProjects() {
-
-        clickOnElement(viewAllProjects);
-        return new AllProjectsPage();
-    }
-
-    
 }
--- src/test/java/project/HomePageTest.java
+++ src/test/java/project/HomePageTest.java
@@ -3,39 +3,21 @@ package project;
 import common.Init;
 import org.junit.jupiter.api.*;
 import pageobjects.HomePage;
-import pageobjects.project.ProjectPage;
 
 class HomePageTest {
 
     private static HomePage homePage;
-    private ProjectPage projectPage;
-    private static String user = "nadezda_peskun@epam.com";
-    private String projectName = "123456789";
+    private static String request = "тестовый запрос";
 
     @BeforeAll
     static void setUp() {
-        homePage = new HomePage(user);
+        homePage = new HomePage();
     }
 
     @Test
-    @DisplayName("Search Project using Project Name filter in Home Page")
+    @DisplayName("Тестирование поиска")
     void testSearchByProjectNameFilter() {
-        projectPage = homePage.goToTopMenu().goToHomePage().filterByProjectName(projectName).openFilteredProject();
-        Assertions.assertEquals(projectPage.getProjectName(), projectName);
-    }
 
-    @Test
-    @DisplayName("Search Project using Project Code filter in Home Page")
-    void testSearchByProjectCodeFilter() {
-        projectPage = homePage.goToTopMenu().goToHomePage().filterByProjectCode(projectName).openFilteredProject();
-        Assertions.assertEquals(projectPage.getProjectName(), projectName);
-    }
-
-    @Test
-    @DisplayName("Search Project using API ID filter in Home Page")
-    void testSearchByApiIdFilter() {
-        projectPage = homePage.goToTopMenu().goToHomePage().filterByApiId(projectName).openFilteredProject();
-        Assertions.assertEquals(projectPage.getProjectName(), projectName);
     }
 
     @AfterAll
