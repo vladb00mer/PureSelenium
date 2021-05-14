2bac4c2c9a217eb632f89515a49dba3eac835c98
Vladislav Murashov <vmurashov@luxoft.com>
2021-05-14 10:37:51 +0300

    GRID additions

--- src/main/java/common/Init.java
+++ src/main/java/common/Init.java
@@ -1,8 +1,14 @@
 package common;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -10,21 +16,50 @@ public class Init {
 
     private static WebDriver driver;
     private static final int TIMEOUT = 10;
+    private static String baseUrlAlpha = "https://sudift.delta.sbrf.ru/meta/gac/";
+    private static String baseUrlSigma = "http://nginx-gac.ci00706314-edevgen-gaac.apps.dev-gen.sigma.sbrf.ru/";
+    private static DesiredCapabilities capabilities;
+    private static ChromeOptions chromeOptions;
 
     public static WebDriver getWebDriver() {
 
-        if(System.getProperty("os.name").startsWith("Windows"))
-            System.setProperty("webdriver.chrome.driver", "./WebDrivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./webdriver/chromedriver.exe");
 
-        else if(System.getProperty("os.name").startsWith("Mac"))
-            System.setProperty("webdriver.chrome.driver", "./WebDrivers/chromedriver");
+        capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.ANY);
+
+        chromeOptions = new ChromeOptions();
+        chromeOptions.merge(capabilities);
 
         if(driver == null) {
             driver = new ChromeDriver();
+            //driver = new RemoteWebDriver(getGridUrl(), chromeOptions);
             driver.manage().window().maximize();
         }
 
         return driver;
+    }
+
+    public static URL getGridUrl() {
+
+        URL url = null;
+
+        try {
+            url = new URL("http://selenium-hub.ci00706314-edevgen-gaac.apps.dev-gen.sigma.sbrf.ru/wd/hub");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getBaseUrlSigma() {
+        return baseUrlSigma;
+    }
+
+    public static String getBaseUrlAlpha() {
+        return baseUrlAlpha;
     }
 
     public static int getTimeOut() {
@@ -32,9 +67,9 @@ public class Init {
         return TIMEOUT;
     }
 
-    public static String getCurrentDateTime() {
+    public String getCurrentDateAdTime(){
 
-        return new SimpleDateFormat("ddMMyy-HHmm").format(Calendar.getInstance().getTime());
+        return new SimpleDateFormat("ddMMyy-Hmm").format(Calendar.getInstance().getTime());
     }
 
     public static void closeDriver() {
@@ -45,3 +80,4 @@ public class Init {
         }
     }
 }
+
