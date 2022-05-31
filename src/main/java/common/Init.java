9030a26013dbc2f9404449e4b12c9af9e97dfbc3
vladb00mer <vla_dick2000@mail.ru>
2022-05-31 17:02:14 +0300

    some new service classes

--- src/main/java/common/Init.java
+++ src/main/java/common/Init.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
